bc5fdba6189bbbb2fc72bf35a89f68323a7c4769
Nurullah <beepbeepwhoisthejeep@gmail.com>
2019-04-03 03:11:13 +0300

    translate

--- js/language.js
+++ js/language.js
@@ -12,13 +12,17 @@
 				lang.adminheader="Bilgi Islem Dairesi Yonetici Paneli";
 				lang.managerheader="Bilgi Islem Dairesi Yetkili Paneli";
 				lang.managerplsunit="Lutfen birim secin!";
+				lang.managerreport="Rapor Alani";
+				lang.managerchoosefile="Eklemek istediginiz dosyayi seciniz";
+				lang.managercheckunitreports ="Birim raporlarini incele";
+				lang.managercommitreport="Raporu isle";
+				lang.managersave="Kaydet";
 				lang.optunit="Biriminiz";
-				lang.copyright="Copyright";
 				lang.del="Sil";
 				lang.admindtusername="Kullanici Adi";
 				lang.admindtname="Isim";
 				lang.admindtsurname="Soyisim";
-        lang.admindtemail="Mail Adresi";
+				lang.admindtemail="Mail Adresi";
 				lang.admindtisactive="Aktif mi?";
 				lang.admindtrolename="Rolu";
 				lang.admindtunitname="Birim adi";
@@ -27,6 +31,7 @@
 				lang.managertabledate=" Raporun Teslim Tarihi";
 				lang.managertablereport="Rapor";
 				lang.admindtdepartmentname="Departman adi";
+				lang.copyright="Akdeniz Universitesi Tum Haklari Saklidir.";
 
         })();
 		
@@ -35,8 +40,12 @@
 				lang.adminheader="Information Departmant Admin Panel";
 				lang.managerheader="Information Departmant Manager Panel";
 				lang.managerplsunit="Please choose your unit!";
+				lang.managerreport="Report Area";
+				lang.managerchoosefile="Choose file";
+				lang.managercheckunitreports ="Check unit reports";
+				lang.managercommitreport="Commit report";
+				lang.managersave="Save";
 				lang.optunit="Your unit";
-				lang.copyright="Copyright";
 				lang.del="Del";
 				lang.admindtusername="Username";
 				lang.admindtname="Name";
@@ -50,14 +59,19 @@
 				lang.managertabledate="Report Date Completed";
 				lang.managertablereport="Report";
 				lang.admindtdepartmentname="Department name";
+				lang.copyright="Akdeniz University All rights reserved.";
 		}
 		lang.translatetr = translatetr;
 		function translatetr(){
 				lang.adminheader="Bilgi Islem Dairesi Yonetici Paneli";
 				lang.managerheader="Bilgi Islem Dairesi Yetkili Paneli";
 				lang.managerplsunit="Lutfen birim secin!";
+				lang.managerreport="Rapor Alani";
+				lang.managerchoosefile="Eklemek istediginiz dosyayi seciniz";
+				lang.managercheckunitreports ="Birim raporlarini incele";
+				lang.managercommitreport="Raporu isle";
+				lang.managersave="Kaydet";
 				lang.optunit="Biriminiz";
-				lang.copyright="Copyright";
 				lang.del="Sil";
 				lang.admindtusername="Kullanici Adi";
 				lang.admindtname="Isim";
@@ -71,6 +85,7 @@
 				lang.managertabledate=" Raporun Teslim Tarihi";
 				lang.managertablereport="Rapor";
 				lang.admindtdepartmentname="Departman adi";
+				lang.copyright="Akdeniz Universitesi Tum Haklari Saklidir.";
 		}
     }
 
